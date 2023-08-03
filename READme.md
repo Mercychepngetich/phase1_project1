ffd6341397e0bc2525f2196da58722df6814c263
Mercy <mercy.chepngetich@student.moringaschool.com>
2023-08-04 01:00:44 +0300

    phase1 project

---
+++ READme.md
@@ -0,0 +1,63 @@
+# Online Hire - Job Posting Platform
+Welcome to Online Hire, a simple web application that allows users to post, search, update, and delete job openings. This README provides an overview of the project and instructions on how to run and use the application effectively.
+
+## Table of Contents
+. Features
+. Installation
+. Usage
+. License
+
+## Features
+Online Hire offers the following features:
+
+. Post a new job with a title and description.
+. View all job openings available on the platform.
+. Search for specific job openings by title.
+. Update the title of an existing job post.
+. Delete a job post.
+. Add comments to job posts.
+
+## Installation
+Follow the instructions below to set up the Online Hire application on your local machine: 
+
+1. Clone the repository from GitHub:
+git clone https://github.com/Mercychepngetich/phase1_project1
+cd **phase1_project1**
+
+2. Open the **index.html** file in your web browser to view the Online Hire application.
+
+## Usage
+
+###  1. View Job Openings:
+
+On the homepage, you can see all the job openings available. Each job posting displays the job title, description, and any existing comments.
+
+### 2. Search Jobs:
+
+Use the search bar labeled "Search Jobs" to search for specific job openings by their titles. The search is case-insensitive and will display matching job posts as you type.
+### 3. Post New Job:
+
+To post a new job opening, scroll down to the "Post New Job" section.
+Enter the job title and description in the respective input fields and click the "Post Job" button.
+If the job is posted successfully, you will receive an alert. Otherwise, you will be informed of the failure.
+### 4. Update Job:
+
+To update the title of an existing job post, scroll down to the "Update Job" section.
+Enter the job ID and the new job title in the respective input fields and click the "Click to Update" button.
+If the update is successful, you will receive an alert, and the job list will be refreshed to reflect the changes.
+### 5. Delete Job:
+
+To delete a job post, scroll down to the "Delete Job" section.
+Enter the job ID you want to delete and click the "Delete" button.
+A confirmation dialog will appear; if you confirm, the job will be deleted from the platform.
+### 6. Add Comment:
+
+To add a comment to a job post, click the "Add Comment" button after every job post. These should showup a form to add comment
+Enter the job ID you want to comment on and your comment text in the respective input fields.
+Click the "Add Comment" button to submit your comment.
+If the comment is added successfully, you will receive an alert, and the job list will be refreshed. However these functionality required more improvement.
+
+##License
+
+Online Hire is licensed under the MIT License. See the LICENSE file for more details.
+
--- index.js
+++ index.js
@@ -1,10 +1,212 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT ||3001;
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT ||3001;
 
-server.use(middlewares);
-server.use(router);
+// server.use(middlewares);
+// server.use(router);
 
-server.listen(port);
+// server.listen(port);
+document.addEventListener("DOMContentLoaded", function (){
+
+function fetchJobs(){
+    fetch("https://phase1-project01.onrender.com/jobs")
+    .then(res => res.json())
+    .then(data => displayJobs(data))
+    .catch(error => console.log(error))
+}
+fetchJobs()
+
+function displayJobs(jobs){
+    for(const job of jobs){
+        const jobPosts = document.getElementById("jobPosts")
+        const newElement = document.createElement("div")
+        newElement.innerHTML = `
+        <div id="className">
+        <h2>${job.title}</h2>
+        Job Post ID:${job.id}
+         <p> ${job.description}</p>
+         <p> ${job.comments}</p>
+         <button> Add Commment</button>
+         </div>
+           `
+        jobPosts.appendChild(newElement)
+//search options
+let options = document.getElementById('datalistOptions')
+let option1 = document.createElement('option')
+option1.innerHTML=`
+<option value = "${job.title}">`
+options.appendChild(option1)
+
+    }
+    
+    function searchJob() {
+        let searchInput = document.getElementById('searchInput').value.toLowerCase();
+        let jobPosts = document.querySelectorAll('#jobPosts > div');
+    
+        for (const jobPost of jobPosts) {
+            let title = jobPost.querySelector('h2').textContent.toLowerCase();
+            if (title.includes(searchInput)) {
+                jobPost.style.display = 'block';
+            } else {
+                jobPost.style.display = 'none';
+            }
+        }
+    }
+    let searchForm = document.getElementById('searchInput');
+searchForm.addEventListener('input', searchJob);
+    
+
+}
+
+
+
+  let form = document.getElementById('postForm')
+       form.addEventListener('submit', postJob )
+      
+
+
+
+//function to post new job
+  
+function postJob(e){
+    e.preventDefault()
+    
+    
+let inputTitle = document.getElementById('title')
+let inputDescription = document.getElementById('description')
+fetch("https://phase1-project01.onrender.com/jobs",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({
+        title:inputTitle.value,
+        description:inputDescription.value
+    })
+} )
+.then((response)=>{
+    if (response.ok){
+        alert("Job has been posted successfully!");
+        form.reset()
+    }else{
+        alert("Failed to post Job. Please try again")
+        
+    }
+})
+
+
+}
+let updateForm = document.getElementById('updateForm');
+updateForm.addEventListener('submit', updateJob);
+
+function updateJob(e) {
+    e.preventDefault();
+
+    let newId = document.getElementById('newId').value;
+    let newTitle = document.getElementById('newtTitle').value;
+
+    fetch(`https://phase1-project01.onrender.com/jobs/${newId}`, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            title: newTitle
+        })
+    })
+        .then((response) => {
+            if (response.ok) {
+                alert("Job has been updated successfully!");
+                updateForm.reset();
+                fetchJobs(); // Refresh the job list after the update
+            } else {
+                alert("Failed to update Job. Please try again");
+                updateForm.reset();
+            }
+        })
+        .catch((error) => {
+            alert("Error occurred while updating job");
+            console.log(error);
+        });
+}
+let deleteForm = document.getElementById('deleteForm');
+deleteForm.addEventListener('submit', deleteJob);
+
+  
+function deleteJob(e){
+    e.preventDefault();
+    let deleteId = document.getElementById('deleteId').value;
+
+    if (confirm("Are you sure you want to delete this job post?")) {
+    fetch(`https://phase1-project01.onrender.com/jobs/${deleteId}`, {
+        method:"DELETE"
+    })
+    .then((response) => {
+        if (response.ok) {
+            alert("Job has been deleted successfully!");
+            deleteForm.reset()
+            fetchJobs(); // job list is refreshed
+        } else {
+            alert("Failed to delete Job. Please try again");
+            deleteForm.reset()
+        }
+    })
+    .catch((error) => {
+        alert("Error occurred while deleting job");
+        console.log(error);
+    });
+
+
+}
+}
+
+
+let commentForm = document.getElementById('commentForm');
+commentForm.addEventListener("submit", function(){
+    addComment()
+})
+
+
+//function to add a comment to job post
+
+function addComment(e){
+    e.preventDefault();
+    let commentId = document.getElementById('commentId').value
+    let commentText = document.getElementById("comment").value
+
+    fetch(`https://phase1-project01.onrender.com/jobs/${commentId}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ comments: commentText })
+    })
+    .then((response) => {
+        if (response.ok) {
+            alert("Comment has been added successfully!");
+            commentForm.reset();
+            fetchJobs(); // Refresh the job list after the comment is added
+        } else {
+            alert("Failed to add comment. Please try again");
+            commentForm.reset();
+        }
+    })
+    .catch((error) => {
+        alert("Error occurred while adding comment");
+        console.log(error);
+    });
+}
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
